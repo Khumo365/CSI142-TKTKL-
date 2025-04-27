2a50b1f80cc2883aeceb85263c67950adde2945b
Khumo365 <202005827@ub.ac.bw>
2025-04-27 15:23:26 +0200

    Update OrderItem.java

--- OrderItem.java
+++ OrderItem.java
@@ -11,12 +11,28 @@ public class OrderItem {
         this.unitPrice = unitPrice;
     }
 
-    public long getId() { return id; }
-    public void setId(long id) { this.id = id; }
-    public Product getProduct() { return product; }
-    public void setProduct(Product product) { this.product = product; }
-    public int getQuantity() { return quantity; }
-    public void setQuantity(int quantity) { this.quantity = quantity; }
-    public double getUnitPrice() { return unitPrice; }
-    public void setUnitPrice(double unitPrice) { this.unitPrice = unitPrice; }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public Product getProduct() {
+        return product;
+    }
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
 }
